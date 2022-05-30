966ef8dd31fc4ebea5f2cb16d4d2d8e7e330a56f
SarahilAnha <rahilanha25@gmail.comm>
2022-05-30 20:58:57 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
+use App\Models\Contact;
+use Illuminate\Support\Facades\Auth;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home' ,[
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "nama" => "Sarahil Anha",
+        "email" => "sarahilanha@gmail.com",
+        "gambar" => "anya.jpg",
+        "title" => "About"
+    ]);
+});
+
+Route::get('/gallery', function () {
+    return view('gallery',[
+        "title" => "Gallery"
+    ]);
+});
+
+Route::get('/home', function(){
+    return view('admin.home', [
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/delete_contact/{id}', ContactController::class . '@destroy');
+
+
+Route::resource('/contact', ContactController::class);
+
+Auth::routes();
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
+
